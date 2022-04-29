6fbd503389f7f578966d45cc9b392afceaed6456
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-29 08:25:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserDetail
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the User Registration Problem");
+        }
+    }
+}
+

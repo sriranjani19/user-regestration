dfa4d36f5b16ee442dca094f13676e6bf6a8cde1
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-29 08:39:02 +0530

    Push rule1

---
+++ Rule_1_program.cs
@@ -0,0 +1,34 @@
+﻿ss
+   
+using System;
+using System.Text.RegularExpressions;
+
+namespace UserPassword
+{
+    class program
+    {
+        public class Pattern
+        {
+            public static string Regex_Pattern = "^[A-Z]{1}[a-z0-9]{4,9}$";
+            public bool DeterminingName(string Password)
+
+            {
+                return Regex.IsMatch(Regex_Pattern, Password);
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter your Password");
+            string Value = Console.ReadLine();
+            bool match = pattern.DeterminingName(Value);
+            if (match)
+                Console.WriteLine("Pattern Matched");
+            else
+            {
+                Console.WriteLine("Pattern not matched");
+            }
+        }
+    }
+}

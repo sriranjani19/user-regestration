3051860a05e06b647546c24f6c2ceced9b5bfd17
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-29 08:27:46 +0530

    Add files via upload

---
+++ LastNameProgram.cs
@@ -0,0 +1,33 @@
+﻿
+   
+using System;
+using System.Text.RegularExpressions;
+
+namespace UserLastName
+{
+    class program
+    {
+        public class Pattern
+        {
+            public static string Regex_Pattern = "^[A-Z]{1}[a-z]{4}$";
+            public bool DeterminingName(string LastName)
+            {
+                return Regex.IsMatch(Regex_Pattern, LastName);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter your Last Name");
+            string Value = Console.ReadLine();
+            bool match = pattern.DeterminingName(Value);
+            if (match)
+                Console.WriteLine("Pattern Matched");
+            else
+            {
+                Console.WriteLine("Pattern not matched");
+            }
+        }
+    }
+}
+
---
+++ MobileProgram.cs
@@ -0,0 +1,33 @@
+﻿
+   
+using System;
+using System.Text.RegularExpressions;
+
+namespace UserMobile
+{
+    class program
+    {
+        public class Pattern
+        {
+            public static string Regex_Pattern = "^[+91]{ }[7-9]{3}[0-9]{7}$";
+            public bool DeterminingName(string Mobile_No)
+
+            {
+                return Regex.IsMatch(Regex_Pattern, Mobile_No);
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter your Mobile Name");
+            string Value = Console.ReadLine();
+            bool match = pattern.DeterminingName(Value);
+            if (match)
+                Console.WriteLine("Pattern Matched");
+            else
+            {
+                Console.WriteLine("Pattern not matched");
+            }
+        }
+    }

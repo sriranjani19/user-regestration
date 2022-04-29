022d146a9e8d84e8434070eb301ce568b30a9f5b
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-29 08:29:36 +0530

    Add files via upload

---
+++ EmailProgram.cs
@@ -0,0 +1,33 @@
+﻿
+   
+using System;
+using System.Text.RegularExpressions;
+
+namespace UserEmail
+{
+    class program
+    {
+        public class Pattern
+        {
+            public static string Regex_Pattern = "^[A-Z]{1,10}(@)(gmail/outlook/yahoo)(.com)$";
+            public bool DeterminingName(string Email)
+            {
+                return Regex.IsMatch(Email, Regex_Pattern);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter your Email Name");
+            string Value = Console.ReadLine();
+            bool match = pattern.DeterminingName(Value);
+            if (match)
+                Console.WriteLine("Pattern Matched");
+            else
+            {
+                Console.WriteLine("Pattern not matched");
+            }
+        }
+    }
+}
+
